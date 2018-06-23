3bd8ff8a3d736bc8993fa2bfd5612ca0f39da5b6
Unknown <natacorteggiano@hotmail.com>
2018-06-23 00:14:41 -0300

    Se coloca ReactStrap y se agrega botón desde Login para direccionar a la Home

--- src/Vistas/Login/Login.js
+++ src/Vistas/Login/Login.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
 import TraerDatos from '../../PruebasFetchPhp/TraerDatos.js';
-
+import { BrowserRouter as Router, Link} from 'react-router-dom';
+import Route from 'react-router-dom/Route';
 
 class Login extends Component {
   render() {
-    return (      
+    return (
+    <Router>      
       <main>       
         <p className="App-intro">
         <input type="text" id="Email"/><br/>
         <input type="password" id="clave"/><br/>
           To get started, edit <code>src/App.js</code> and save to reload.  Main        
         </p>     
-        
+        <Link to="/home">Home</Link>
       </main>
+      </Router>
     );
   }
 }
